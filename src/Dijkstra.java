92f729f9b4462121ccd96979d86fba30fd0f5232
Abhishek Gupta <ag4cb@virginia.edu>
2015-12-12 19:25:34 -0500

    Finish input parsing change

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -14,21 +14,25 @@ public class Dijkstra {
 	public static void main(String[] args) throws FileNotFoundException {
 		/* read input from text file to set up graph */
 		readInput("datasets/input6.txt");
-		System.out.println("Hello world! I successfully read in data");
-		transformDataSet();
-		System.out.println("Hello world! I successfully set up graph. Running Dijkstra...");
+		//displayGraph();
 		// start with node 0
 		Node source = Graph.get(0);
 		runDijkstra(source);
 	}
 
-	private static void transformDataSet() {
+	private static void displayGraph() {
+		System.out.println("Number of nodes: " + noNodes);
+		System.out.println("Number of edges: " + noEdges);
+
+		for (Node n : Graph)
+			System.out.println(n.id + "  " + n.xCoord + "  " + n.yCoord);
+
 		for (Node n : Graph) {
-			for (Node adj : n.adjList) {
-				double edgeWeight = edges.get("" + n.id + adj.id);
-				n.neighbors.put(adj, edgeWeight);
+			for (Node x : n.neighbors.keySet()) {
+				System.out.println(n.id + "  " + x.id);
 			}
 		}
+
 	}
 
 	private static void runDijkstra(Node source) {
@@ -53,29 +57,27 @@ public class Dijkstra {
 
 	private static void readInput(String filename) throws FileNotFoundException {
 		Scanner inputScanner = new Scanner(new File(filename));
-		String[] vals = inputScanner.nextLine().split(" ");
-		noNodes = Integer.parseInt(vals[0]);
-		noEdges = Integer.parseInt(vals[1]);
-		for (int i = 0; i < noNodes; i += 1) {
-			String[] values = inputScanner.nextLine().split(" ");
-			Graph.add(new Node(Integer.parseInt(values[0]), Integer
-					.parseInt(values[1]), Integer.parseInt(values[2])));
+		ArrayList<Integer> val = new ArrayList<Integer>();
+		while (inputScanner.hasNextInt())
+			val.add(inputScanner.nextInt());
+
+		noNodes = val.get(0);
+		noEdges = val.get(1);
+
+		int i;
+		for (i = 2; i < 2 + 3 * noNodes; i += 3) {
+			int id = val.get(i);
+			int xCoord = val.get(i + 1);
+			int yCoord = val.get(i + 2);
+			Graph.add(new Node(id, xCoord, yCoord));
 		}
-		inputScanner.nextLine();
-		for (int j = 0; j < noEdges; j += 1) {
-			String[] node = inputScanner.nextLine().split(" ");
-			int[] nodes = new int[node.length];
-			for (int i = 0; i < nodes.length; i += 1)
-				nodes[i] = Integer.parseInt(node[i]);
-			Node node1 = Graph.get(nodes[0]);
-			Node node2 = Graph.get(nodes[1]);
-			node1.adjList.add(node2);
-			node2.adjList.add(node1);
+		int j;
+		for (j = i; j < i + 2 * noEdges; j += 2) {
+			Node node1 = Graph.get(val.get(j));
+			Node node2 = Graph.get(val.get(j + 1));
 			double distance = compDistance(node1, node2);
-			String key1 = "" + node1.id + node2.id;
-			String key2 = "" + node2.id + node1.id;
-			edges.put(key1, distance);
-			edges.put(key2, distance);
+			node1.neighbors.put(node2, distance);
+			node2.neighbors.put(node1, distance);
 		}
 	}
 
